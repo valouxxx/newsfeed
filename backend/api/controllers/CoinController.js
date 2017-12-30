c1923d114bcf0a0b3cbd0a0780819dc315c98d2d
valouxxx <mycryptonewsfeed@vps1062.fr.ns.planethoster.net>
2017-12-30 20:41:20 +0200

    coin api

---
+++ backend/api/controllers/CoinController.js
@@ -0,0 +1,11 @@
+/**
+ * CoinController
+ *
+ * @description :: Server-side logic for managing coins
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
